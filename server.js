3aaa8b3bff62bd0facf4213361f3f008ed424615
anti_yurta <amarbat.gurensoft@gmail.com>
2022-08-24 16:00:20 +0800

    cors

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to GurenSoft application." });
 });
 
 // routes
